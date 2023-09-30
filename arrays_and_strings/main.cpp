5eb73a3c05151b4027516abeae253094d324dd61
Timothy <costigt@tcd.ie>
2023-09-30 14:28:21 +0100

    use numeric_limits, brain fart of using sizeof() removed

--- arrays_and_strings/main.cpp
+++ arrays_and_strings/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <limits>
 #include <map>
 #include <catch2/catch_all.hpp>
 
@@ -21,7 +22,7 @@ bool is_unique(std::string str) {
 
 bool is_unique_alt(std::string str)
 {
-	bool chars_encountered[sizeof(char)] = { false };
+	bool chars_encountered[std::numeric_limits<char>::max()] = {false};
 	for (int i = 0; i < str.size(); i++) {
 		char c = str[i];
 		if (chars_encountered[c] == true) {
@@ -35,7 +36,7 @@ bool is_unique_alt(std::string str)
 	return true;
 }
 
-TEST_CASE("is_unique_test")
+TEST_CASE("is_unique")
 {
 	std::string str1 = "abcdef";
 	REQUIRE(is_unique(str1) == true);
@@ -44,4 +45,52 @@ TEST_CASE("is_unique_test")
 	std::string str2 = "aabcdef";
 	REQUIRE(is_unique(str2) == false);
 	REQUIRE(is_unique_alt(str2) == false);
+}
+
+// 1.2 check permutation
+// if different lengths - not permutations
+// if have any different characters - not permutations
+// need to have same characters, in same frequency
+bool is_permutation(std::string str1, std::string str2)
+{
+	if (str1.size() != str2.size()) {
+		return false;
+	}
+
+	int str1_char_freq[std::numeric_limits<char>::max()] = { 0 };
+	int str2_char_freq[std::numeric_limits<char>::max()] = { 0 };
+
+	for (int i = 0; i < str1.size(); i++) {
+		str1_char_freq[str1[i]]++;
+		str2_char_freq[str2[i]]++;
+	}
+
+	for (int i = 0; i < std::numeric_limits<char>::max(); i++) {
+		if (str1_char_freq[i] != str2_char_freq[i]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+TEST_CASE("is_permutation")
+{
+	{
+		std::string str1 = "helloworld";
+		std::string str2 = "worldhello";
+		REQUIRE(is_permutation(str1, str2) == true);
+	}
+
+	{
+		std::string str1 = "helloworld";
+		std::string str2 = "worldHello";
+		REQUIRE(is_permutation(str1, str2) == false);
+	}
+	
+	{
+		std::string str1 = "helloworld";
+		std::string str2 = "Hello,world!";
+		REQUIRE(is_permutation(str1, str2) == false);
+	}
 }
